0fd552e8e9a021153987c693209c00f1ef15235a
Tinuola Adeyemi <Tinucy@yahoo.com>
2023-11-21 19:02:31 +0000

    completed hook and stateuse of the registration page

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,41 +1,77 @@
-import React from 'react';
-import '../styles/form.scss';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/form.scss";
+import { Link } from "react-router-dom";
 
 export default function register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
-    <div class="form form--page">
-      <div class="form__group form__group--page">
-        <label class="form__label">First name</label>
-        <input class="form__field" type="text" placeholder="First name" />
+    <div className="form form--page">
+      <div className="form__group form__group--page">
+        <label className="form__label">First name</label>
+        <input
+          className="form__field"
+          type="text"
+          placeholder="First name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Last name</label>
-        <input class="form__field" type="text" placeholder="Last name" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Last name</label>
+        <input
+          className="form__field"
+          type="text"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Email</label>
-        <input class="form__field" type="text" placeholder="Email" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Email</label>
+        <input
+          className="form__field"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Choose password</label>
-        <input class="form__field" type="text" placeholder="Choose password" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Choose password</label>
+        <input
+          className="form__field"
+          type="text"
+          placeholder="Choose password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Confirm Password</label>
-        <input class="form__field" type="text" placeholder="Confirm Password" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Confirm Password</label>
+        <input
+          className="form__field"
+          type="text"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => set(e.target.value)}
+        />
       </div>
 
-      <div class="form__group form__group--page">
-        <input class="form__btn" type="submit" value="Register" />
+      <div className="form__group form__group--page">
+        <input className="form__btn" type="submit" value="Register" />
       </div>
 
       <footer>
-        Already have an account? <Link to='/login'>Login</Link>
+        Already have an account? <Link to="/login">Login</Link>
       </footer>
     </div>
   );
